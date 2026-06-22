b3b386749965d6fee715a29e2d7bf841a3d6f911
yy675908-rgb <yy675908@gmail.com>
2026-06-22 15:08:21 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'study-org-v2';
+const CACHE = 'study-org-v4';
 const SHELL = ['./','index.html','manifest.json'];   // 外壳：必定存在
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
